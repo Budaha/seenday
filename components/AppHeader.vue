93ef6abe9f92ce7c09a7929b45b779889e213c32
Andrey <budahinan@gmail.com>
2024-06-23 13:41:49 +0300

    feat(app): components and scss

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,9 +1,44 @@
 <template>
-  <div>
-    Header
+  <div class="nav">
+    <div class="nav__image">
+      <img src="../assets/image/Seenday.png">
+    </div>
+    <div class="nav__text">
+      <p>Работа с Seenday</p>
+      <p>Автоматизация</p>
+      <p>Тарифы</p>
+      <p>Контакты</p>
+    </div>
+    <div class="nav__btn">
+      <button>Личный кабинет</button>
+    </div>
   </div>
 </template>
   
 <script setup lang="ts">
   
 </script>
+
+<style scoped lang="scss">
+.nav {
+  display: flex;
+  &__image {
+    padding-left: 5px;
+    img {
+      width: 264.89px;
+      height: 57.6px;
+    }
+  }
+  &__text {
+    display: flex;
+    gap: 0 10px;
+    p {
+      font-size: 17px;
+      line-height: 20.72px;
+      align-items: center;
+      font-weight: 500;
+      color: #282828;
+    }
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,20 @@
 <template>
-  <div>
-    <AppHeader/>
+    <div class="header">
+      <AppHeader class="container"/>
+    </div>
     <slot />
-    <AppFooter/>
-  </div>
+    <div class="footer">
+      <AppFooter class="container"/>
+    </div>
 </template>
 
 <script setup lang="ts">
 import { AppFooter, AppHeader } from '#components'
 
 </script>
+
+<style lang="scss" scoped>
+.header {
+  padding-top: 20px;
+}
+</style>
